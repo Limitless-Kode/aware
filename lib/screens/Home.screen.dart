25789183e94660aea471ffa7deb9561614a1eeea
Peter Claver <limitless.claver@gmail.com>
2020-05-31 09:10:39 +0000

    Animate to my location is implemented at this point

--- lib/screens/Home.screen.dart
+++ lib/screens/Home.screen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
 
 
 class Home extends StatefulWidget {
@@ -12,13 +12,29 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  Completer<GoogleMapController> _controller = Completer();
-  Position position;
+  GoogleMapController mapController;
+  Location _location = Location();
+  LocationData _locationData;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
   getLocation() async{
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    setState(() {
-      this.position = position;
+    final MarkerId markerId = MarkerId('0');
+
+    _location.onLocationChanged.listen((LocationData currentLocation) {
+      final Marker marker = Marker(
+        markerId: markerId,
+        position: LatLng(
+          currentLocation.latitude,
+          currentLocation.longitude,
+        ),
+      );
+      LatLng latLng = LatLng(currentLocation.latitude, currentLocation.longitude);
+      CameraUpdate cameraUpdate = CameraUpdate.newLatLngZoom(latLng, 15);
+      mapController.animateCamera(cameraUpdate);
+      setState(() {
+        this._locationData = currentLocation;
+        markers[markerId] = marker;
+      });
     });
   }
 
@@ -32,19 +48,33 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        myLocationEnabled: true,
-        mapToolbarEnabled: true,
-        zoomControlsEnabled: false,
-        myLocationButtonEnabled: false,
-        mapType: MapType.normal,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(this.position?.latitude ?? 6.7008168, this.position?.longitude ?? -1.6998494),
-          zoom: 14.4746,
-        ),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            myLocationEnabled: true,
+            mapToolbarEnabled: true,
+            zoomControlsEnabled: false,
+            myLocationButtonEnabled: false,
+            mapType: MapType.normal,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(this._locationData?.latitude ?? 6.7008168, this._locationData?.longitude ?? -1.6998494),
+              zoom: 14.4746,
+            ),
+            onMapCreated: (GoogleMapController controller) {
+              setState(() {
+                mapController = controller;
+              });
+            },
+            markers: Set<Marker>.of(markers.values),
+          ),
+          Positioned(
+            child: Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.white,
+              child: Text("LAT: ${this._locationData?.latitude ?? 0}  LONG: ${this._locationData?.longitude ?? 0}"),
+            ),
+          )
+        ],
       ),
     );
   }
